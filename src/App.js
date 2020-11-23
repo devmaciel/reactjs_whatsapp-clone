cddd735fa922c826ba074edd29531b3780ff54ef
João Pedro Maciel <joomaciiel2@outlook.com>
2020-11-22 22:54:17 -0300

    login with google firebase auth

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
+import { useState } from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import Chat from "./Components/Chat/Chat";
+import Login from "./Components/Login/Login";
 import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
+	const [user, setUser] = useState(null);
+
 	return (
 		// BEM naming convention
 		<div className="app">
-			<div className="app__body">
-				<Router>
-					<Switch>
+			{!user ? (
+				<Login />
+			) : (
+				<div className="app__body">
+					<Router>
 						<Sidebar />
 
-						<Route path="/rooms/:roomId">
-							<Chat />
-						</Route>
+						<Switch>
+							<Route path="/rooms/:roomId">
+								<Chat />
+							</Route>
 
-						<Route path="/">
-							<Chat />
-						</Route>
-					</Switch>
-				</Router>
-			</div>
+							<Route path="/">
+								<Chat />
+							</Route>
+						</Switch>
+					</Router>
+				</div>
+			)}
 		</div>
 	);
 }
--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -9,19 +9,28 @@ import {
 import React, { useEffect, useState } from "react";
 import "./Chat.css";
 import { useParams } from "react-router-dom";
+import db from "../../firebase/firebase";
 
 function Chat() {
 	const [seed, setSeed] = useState("");
 	const [input, setInput] = useState("");
 	const { roomId } = useParams();
+	const [roomName, setRoomName] = useState("");
+
+	useEffect(() => {
+		if (roomId) {
+			db.collection("rooms")
+				.doc(roomId)
+				.onSnapshot((snapshot) => setRoomName(snapshot.data().name));
+		}
+	}, [roomId]);
 
 	useEffect(() => {
 		setSeed(Math.floor(Math.random() * 5000));
-	}, []);
+	}, [roomId]);
 
 	const sendMessage = (e) => {
 		e.preventDefault();
-		// console.log("You typed ->", input);
 		setInput("");
 	};
 
@@ -33,7 +42,7 @@ function Chat() {
 				/>
 
 				<div className="chat__headerInfo">
-					<h3>Room name</h3>
+					<h3>{roomName}</h3>
 					<p>Last seen at ...</p>
 				</div>
 
